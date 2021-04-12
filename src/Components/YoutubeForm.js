8d999325246a1915ed37bd6170d392072805d87c
nihalshanp <pnihalshan94@gmail.com>
2021-04-12 16:24:51 +0530

    learned state management

--- src/Components/YoutubeForm.js
+++ src/Components/YoutubeForm.js
@@ -2,23 +2,51 @@ import React from "react";
 import { useFormik } from "formik";
 
 const YoutubeForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit: values => {
+      console.log("form data", values)
+    }
+  });
 
-    const formik = useFormik({})
+  // console.log("formik values", formik.values);
 
   return (
     <div>
-      <form>
+      <form onSubmit={formik.handleSubmit} >
         <label htmlFor="name"> Name </label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
         <label htmlFor="email">E-Mail</label>
-        <input type="email" name="email" id="email" />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" name="channel" id="channel" />
+        <input
+          type="text"
+          name="channel"
+          id="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
 
         <button
-        // type="submit"
+        type="submit"
         >
           Submit
         </button>
