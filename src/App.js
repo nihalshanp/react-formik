4afb5d6face36d04336ad70e1428c55d778e6068
nihalshanp <pnihalshan94@gmail.com>
2021-04-12 15:24:33 +0530

    added use Formik

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import YoutubeForm from "./Components/YoutubeForm";
 
 function App() {
-  return <div className="App">
-    <YoutubeForm />
-  </div>;
+  return (
+    <div className="App">
+      <YoutubeForm />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/YoutubeForm.js
+++ src/Components/YoutubeForm.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const YoutubeForm = () => {
+
+    const formik = useFormik({})
+
   return (
     <div>
       <form>
